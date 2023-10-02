2e0a9e05ceb8254d4eebcdb17dc96ffbe6702eb4
腹黒い茶 <haraguroicha@tipsy.coffee>
2023-10-03 00:08:52 +0800

    add caffeine 1.2.2

---
+++ Casks/caffeine.rb
@@ -0,0 +1,11 @@
+cask "caffeine" do
+  name "Caffeine"
+  desc "Keeps your Mac awake. Caffeine is a tiny open source program that puts an icon in the right side of your menubar. Click it to prevent your Mac from automatically going to sleep, dimming the screen or starting screen savers."
+  homepage "https://www.caffeine-app.net/"
+  url "https://www.caffeine-app.net/download/"
+
+  version "1.2.2"
+  sha256 "a256120d17983c89de3bdacd97f3e84e488c9782e6b2365ced365593a29beee5"
+
+  app "Caffeine.app"
+end
